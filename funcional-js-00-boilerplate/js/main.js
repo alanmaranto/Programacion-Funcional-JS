3319ab56043b6f42a3e0eafdf0448679263d8076
alanmaranto <alanarostegui@hotmail.com>
2019-10-06 16:09:50 -0500

    Feat: Validate Inputs

--- funcional-js-00-boilerplate/js/main.js
+++ funcional-js-00-boilerplate/js/main.js
@@ -1,15 +1,31 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
 
-// const CALORIES = document.getElementById('calories');
-// const DESCRIPTION = document.getElementById('description');
-// const CARBS = document.getElementById('carbs');
-// const PROTEIN = document.getElementById('protein');
+const CALORIES = document.getElementById('calories');
+const DESCRIPTION = document.getElementById('description');
+const CARBS = document.getElementById('carbs');
+const PROTEIN = document.getElementById('protein');
 
-// console.log(`Calories: ${CALORIES.value}`);
-let { 
-  description,
-  calories,
-  carbohydrates,
-  protein 
-} = document.getElementsByTagName('input');
+// const { 
+//   CALORIES,
+//   DESCRIPTION,
+//   CARBS,
+//   PROTEIN 
+// } = document.getElementsByTagName('input');
+const errorClass = 'is-invalid'
+
+DESCRIPTION.addEventListener('keypress', () => DESCRIPTION.classList.remove(errorClass))
+CALORIES.addEventListener('keypress', () => CALORIES.classList.remove(errorClass))
+CARBS.addEventListener('keypress', () => CARBS.classList.remove(errorClass))
+PROTEIN.addEventListener('keypress', () => PROTEIN.classList.remove(errorClass))
+
+const validateInputs = () => {
+  DESCRIPTION.value ? '' : DESCRIPTION.classList.add(errorClass)
+  CALORIES.value ? '' : CALORIES.classList.add(errorClass)
+  CARBS.value ? '' : CARBS.classList.add(errorClass)
+  PROTEIN.value ? '' : PROTEIN.classList.add(errorClass)
+
+  if (DESCRIPTION.value && CALORIES.value && CARBS.value && PROTEIN.value) {
+    console.log('ok')
+  }
+}
